15917c26b93350f214b9467b1766557e65d23cf7
Balaji Heritage Lab <balajiheritagelab@gmail.com>
2026-02-20 23:07:05 +0530

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,28 +1,50 @@
 let db;
+let dbReady = false;
 
 function initDB() {
-  const request = indexedDB.open("TKS_DB", 1);
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open("TKS_DB", 1);
 
-  request.onupgradeneeded = function (e) {
-    db = e.target.result;
-    db.createObjectStore("records", { keyPath: "uuid" });
-  };
+    request.onupgradeneeded = function (e) {
+      db = e.target.result;
+      if (!db.objectStoreNames.contains("records")) {
+        db.createObjectStore("records", { keyPath: "uuid" });
+      }
+    };
 
-  request.onsuccess = function (e) {
-    db = e.target.result;
-  };
+    request.onsuccess = function (e) {
+      db = e.target.result;
+      dbReady = true;
+      resolve();
+    };
+
+    request.onerror = function () {
+      reject("IndexedDB failed");
+    };
+  });
 }
 
 function saveRecord(record) {
+  if (!dbReady) {
+    alert("Database not ready yet.");
+    return;
+  }
+
   const tx = db.transaction(["records"], "readwrite");
   const store = tx.objectStore("records");
   store.put(record);
 }
 
 function getAllRecords(callback) {
+  if (!dbReady) {
+    alert("Database not ready yet.");
+    return;
+  }
+
   const tx = db.transaction(["records"], "readonly");
   const store = tx.objectStore("records");
   const request = store.getAll();
+
   request.onsuccess = () => callback(request.result);
 }
 
